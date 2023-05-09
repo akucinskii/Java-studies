9126b5f03f5c801647dd23429883991171aabefe
akucinski <artuyees@gmail.com>
2023-05-09 21:44:22 +0200

    refactor: removed old files

--- src/Osobowy.java
+++
@@ -1,7 +0,0 @@
-public class Osobowy extends Pojazd {
-    public Osobowy(int konieMechaniczne) {
-        super(konieMechaniczne, "osobowy" );
-    }
-
-
-}
--- src/Star.java
+++
@@ -1,6 +0,0 @@
-public class Star extends Ciezarowy {
-    public Star(int konieMechaniczne, int udzwig, int towar) {
-        super(konieMechaniczne, udzwig, towar);
-    }
-
-}
